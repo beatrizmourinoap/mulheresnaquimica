4bc68860ff5ddc5b935ce4d572f72dd3161e119b
beatrizmourinoap <50416732+beatrizmourinoap@users.noreply.github.com>
2019-05-26 17:59:38 -0300

    Add files via upload

--- actinium-page.php
+++ actinium-page.php
@@ -1,1 +1,2 @@
-<?php include_once("actinium-page.html"); header('Content-Type: text/html; charset=ISO-8859-1'); ?>
+<?php include_once("actinium-page.html"); echo htmlspecialchars($string, ENT_COMPAT,'ISO-8859-1', true);
+echo htmlentities($string, ENT_COMPAT,'ISO-8859-1', true); ?>
