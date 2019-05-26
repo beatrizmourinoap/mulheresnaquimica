b48c9c576a3467c210401b34d8b5afc278d91d3e
beatrizmourinoap <50416732+beatrizmourinoap@users.noreply.github.com>
2019-05-26 18:04:46 -0300

    Add files via upload

--- actinium-page.php
+++ actinium-page.php
@@ -1,3 +1,6 @@
-<?php include_once("actinium-page.html"); header("Content-Type: text/html;
+<?php 
+header("Content-Type: text/html;
 charset=utf-8");
-ini_set("default_charset", 'utf-8'); ?>
+ini_set("default_charset", 'utf-8'); 
+include_once("actinium-page.html"); 
+?>
